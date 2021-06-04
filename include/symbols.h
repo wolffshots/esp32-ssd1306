3b3bb2eab4de4a3523202252b7234f3c5d3261e7
jadon <16850875+wolffshots@users.noreply.github.com>
2021-06-04 23:46:19 +0200

    added new fonts and symbols

---
+++ include/symbols.h
@@ -0,0 +1,16 @@
+#ifndef SSD1306_SYMBOLS
+#define SSD1306_SYMBOLS
+
+// custom symbols
+static uint8_t degree_symbol[8] = {
+    0x00,
+    0x00,
+    0x00,
+    0x0C,
+    0x12,
+    0x12,
+    0x0C,
+    0x00,
+};
+
+#endif // SSD1306_SYMBOLS
